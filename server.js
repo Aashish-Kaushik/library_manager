eb41b16a37562a6bf27f89853a3c7599cc385f6e
Aashish-Kaushik <ashishkaushik8126@gmail.com>
2023-09-16 16:30:43 +0530

    add swagger docs

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const express = require("express");
 const userRoutes = require("./routes/user.route");
 const bookRouter = require("./routes/book.router");
 const connectDB = require("./config/db");
-
+const swaggerUi = require("swagger-ui-express");
+const yaml = require("yamljs");
+const swaggerDocumentation = yaml.load("./swagger.yaml");
 const app = express();
 
-connectDB();
+// connectDB();
 
 app.use(express.json());
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocumentation));
 app.use("/user", userRoutes);
 app.use("/book", bookRouter);
 
